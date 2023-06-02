9089a614b8dd96b51bda877ce95af2c0e05cf59c
lpoto <luka.potochnik@gmail.com>
2023-06-02 16:21:57 +0200

    Improve containers and images displayers

--- lua/telescope-docker/pickers/containers/finder.lua
+++ lua/telescope-docker/pickers/containers/finder.lua
@@ -2,18 +2,26 @@ local finders = require "telescope.finders"
 local entry_display = require "telescope.pickers.entry_display"
 
 local get_container_display
+local max_name_width = 10
 
 ---Create a telescope finder for the currently available containers.
 ---
 ---@param containers_tbl Container[]
 ---@return table?: a telescope finder
 local function containers_finder(containers_tbl)
+  for _, container in ipairs(containers_tbl or {}) do
+    local name = container.Names
+    if #name + 3 > max_name_width then
+      max_name_width = #name + 3
+    end
+  end
+
   return finders.new_table {
     results = containers_tbl,
     entry_maker = function(entry)
       return {
         value = entry,
-        ordinal = entry.Names,
+        ordinal = entry.State .. " " .. entry.Names .. " " .. entry.ID,
         display = function(entry2)
           return get_container_display(entry2.value)
         end,
@@ -28,6 +36,7 @@ get_container_display = function(container)
     separator = " ",
     items = {
       { width = 8 },
+      { width = max_name_width },
       { remaining = true },
     },
   }
@@ -40,6 +49,7 @@ get_container_display = function(container)
   return displayer {
     { container.State, hl },
     container.Names,
+    { container.ID, "Comment" },
   }
 end
 
--- lua/telescope-docker/pickers/images/finder.lua
+++ lua/telescope-docker/pickers/images/finder.lua
@@ -2,18 +2,25 @@ local finders = require "telescope.finders"
 local entry_display = require "telescope.pickers.entry_display"
 
 local get_image_display
+local max_name_width = 10
 
 ---Create a telescope finder for the currently available images.
 ---
 ---@param images_tbl Image[]
 ---@return table
 local function images_finder(images_tbl)
+  for _, image in ipairs(images_tbl or {}) do
+    local name = image:name()
+    if #name + 3 > max_name_width then
+      max_name_width = #name + 3
+    end
+  end
   return finders.new_table {
     results = images_tbl,
     entry_maker = function(entry)
       return {
         value = entry,
-        ordinal = entry:name(),
+        ordinal = entry:name() .. " " .. entry.ID,
         display = function(entry2)
           return get_image_display(entry2.value)
         end,
@@ -26,10 +33,11 @@ end
 get_image_display = function(image)
   local displayer = entry_display.create {
     separator = " ",
-    items = { {} },
+    items = { { width = max_name_width }, {} },
   }
   return displayer {
     image:name(),
+    { image.ID, "Comment" },
   }
 end
 
